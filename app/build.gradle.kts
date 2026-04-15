80864ae9d6a83b94c4305c07010d00f67bad43b9
sslaia <laia.sirus@gmail.com>
2026-04-15 14:29:51 +0100

    first release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.blogspot.niaskeyboard"
         minSdk = 21
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
